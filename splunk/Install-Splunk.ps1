ce9c688d4e831eee0ada15e5605fd062a33110c2
Kaleb Hawkins <KalebHawkins@outlook.com>
2023-02-07 15:11:58 -0600

    add splunk uf install script

---
+++ splunk/Install-Splunk.ps1
@@ -0,0 +1,54 @@
+Function Install-Splunk {
+<#
+.Synopsis
+    Install the Splunk Forwarder.
+.DESCRIPTION
+    Install the Splunk Universal forwarder. 
+.EXAMPLE
+    Install-Splunk.ps1 -ComputerName Comp1,Comp2,Comp3 -Installer C:\Path\To\Installer.msi -SplunkUsername user -SplunkPassword pass -DeploymentServer SplunkDeploy.domain.com:8089
+#>
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline)]
+        [ParameterType]
+        [string[]]$ComputerName,
+        [Parameter()]
+        [ParameterType]
+        [string]$Installer,
+        [Parameter()]
+        [ParameterType]
+        [string]$SplunkUsername,
+        [Parameter()]
+        [ParameterType]
+        [string]$SplunkPass,
+        [Parameter()]
+        [ParameterType]
+        [string]$DeploymentServer
+        
+    )
+    
+    begin {}
+    process {
+        [ScriptBlock]$ScriptBlock = {
+            param(
+                $InstallerPath,
+                $SplunkUsername,
+                $SplunkPass,
+                $DeploymentServer
+            )
+
+            $installerPath = "C:\temp\$InstallerPath"
+
+            msiexec /i $InstallerPath AGREETOLICENSE=yes DEPLOYMENT_SERVER=$DeploymentServer SPLUNKUSERNAME=$SplunkUsername SPLUNKPASSWORD=$SplunkPass /quiet /L*v C:\Temp\Splunk_install.log
+        }
+
+        foreach ($Computer in $ComputerName) {
+            $InstallerName = Split-Path $Installer -Leaf
+            Copy-Item -Path $Installer -Destination \\$Computer\C$\temp\$InstallerName
+            
+            $s = New-PSSession -ComputerName $Computer
+            Invoke-Command -Session $s -ScriptBlock $ScriptBlock -ArgumentList $InstallerName, $SplunkUsername, $SplunkPass, $DeploymentServer
+            Remove-PSSession -Session $s
+        }
+    }
+}
